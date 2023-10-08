5e17c1932c9d415a298c9279d189e2fc17081e4b
Shanu044 <shanupaliwal83@gmail.com>
2023-10-07 20:59:25 -0400

    Finihsed Calculator

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MultiCalculator"
+include(":androidApp")
+include(":shared")
